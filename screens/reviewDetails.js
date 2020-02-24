a171ea043e7f97cb25f7e79160de6e74fc5ce301
adyprajwal <prajwal.ady@gmail.com>
2020-02-24 12:30:53 +0545

    pass parameters to routes

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
-export default function ReviewDetails() {
+
+export default function ReviewDetails({ route, navigation }) {
+	const { title, body, rating } = route.params;
 	return (
 		<View style={globalStyles.container}>
-			<Text>Review Details Screen</Text>
+			<Text>{title}</Text>
+			<Text>{body}</Text>
+			<Text>{rating}</Text>
 		</View>
 	);
 }
